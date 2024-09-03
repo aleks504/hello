db66a59bb0adf660eebe651975beb7049cd40ad4
aleks504 <78157961+aleks504@users.noreply.github.com>
2024-09-03 15:32:35 -0500

    added my major to the output

---
+++ hello.c
@@ -0,0 +1,20 @@
+/**
+ * Author: Aleksandra Stansbury
+ *         astansbury9@huskers.unl.edu
+ * Date: 2024/09/03
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+
+  printf("Aleksandra Stansbury!\n");
+  printf("Computer Engineering\n");
+
+
+  return 0;
+}
